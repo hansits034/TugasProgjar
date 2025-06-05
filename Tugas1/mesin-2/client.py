82b33fcbfb23f473de256e1850a9823725051a2e
hansits034 <162028121+hansits034@users.noreply.github.com>
2025-06-05 21:41:10 +0700

    Create client.py

---
+++ Tugas1/mesin-2/client.py
@@ -0,0 +1,32 @@
+import socket
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
+try:
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server_address = ('0.0.0.0', 32444)
+    logging.info(f"starting up on {server_address}")
+    sock.bind(server_address)
+    sock.listen(1)
+
+    while True:
+        logging.info("waiting for a connection")
+        connection, client_address = sock.accept()
+        logging.info(f"connection from {client_address}")
+        while True:
+            data = connection.recv(32)
+            logging.info(f"received {data}")
+            if data:
+                logging.info("sending back data")
+                connection.sendall(data)
+            else:
+                break
+        connection.close()
+
+except Exception as e:
+    logging.info(f"ERROR: {str(e)}")
+finally:
+    logging.info("closing")
+    sock.close()
